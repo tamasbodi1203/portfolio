7acb1b8b8aa184d198a3592576c8c8bfe16ddbe0
Bódi Tamás <h751255@stud.inf.u-szeged.hu>
2021-11-04 21:03:14 +0100

    Initialize Spring

---
+++ src/test/java/hu/portfoliotracker/PortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.portfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
