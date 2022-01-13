eccfe5cff953346ab976ec38702a5cc81c2a0900
Bódi Tamás <h751255@stud.inf.u-szeged.hu>
2022-01-13 20:14:49 +0100

    Implement async refresh button

--- src/main/java/hu/portfoliotracker/Controller/ContentController.java
+++ src/main/java/hu/portfoliotracker/Controller/ContentController.java
@@ -5,31 +5,35 @@ import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PortfolioController {
+@RequestMapping("balance")
+public class ContentController {
 
     @Autowired
     private PortfolioService portfolioService;
 
-    @GetMapping
-    public String listOfPositions(Model model) {
+    @RequestMapping("")
+    public String loadContent() {
+        return "website";
+    }
 
+    @RequestMapping("content1")
+    public String getContent1() {
+        return "balance :: content1";
+    }
 
+    @RequestMapping("content2")
+    public String getContent2(Model model) {
         val portfolioDtos = portfolioService.refreshPortfolio();
         model.addAttribute("spotBalanceDto", portfolioDtos.get(0));
         model.addAttribute("crossBalanceDto", portfolioDtos.get(1));
         model.addAttribute("isolatedBalanceDto", portfolioDtos.get(2));
 
         model.addAttribute("currency", "$");
-        return "home";
-    }
 
-    @RequestMapping("content1")
-    public String getContent1() {
-        return "balance :: content1";
+        return "balance :: content2";
     }
 
 }
