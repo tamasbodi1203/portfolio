f400d1414c30c47155285cc1757923c69c84efb3
Bódi Tamás <h751255@stud.inf.u-szeged.hu>
2022-04-03 19:21:27 +0200

    Create dynamic pie chart for spot positions

--- src/main/java/hu/portfoliotracker/Controller/GoogleChartController.java
+++ src/main/java/hu/portfoliotracker/Controller/GoogleChartController.java
@@ -1,19 +1,25 @@
 package hu.portfoliotracker.Controller;
 
 import hu.portfoliotracker.DTO.CountCourse;
+import hu.portfoliotracker.Service.PortfolioService;
 import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 @Controller
 @RequestMapping("/google-chart")
 public class GoogleChartController {
+
+    @Autowired
+    private PortfolioService portfolioService;
 
     private static final Random RANDOM = new Random(System.currentTimeMillis());
 
@@ -27,7 +33,7 @@ public class GoogleChartController {
         pieChartData.add(cc2);
         pieChartData.add(cc3);
 
-        model.addAttribute("chartData", getSpotChartData());
+        model.addAttribute("chartData", getDymaicSpotChartData());
         return "google-chart";
     }
 
@@ -51,5 +57,17 @@ public class GoogleChartController {
                 List.of("MANA", 249.00),
                 List.of("PYR", 249.98)
         );
+    }
+
+    private List<List<Object>> getDymaicSpotChartData() {
+        val listOfLists = new ArrayList<List<Object>>();
+        val portfolioDtos = portfolioService.refreshPortfolio();
+        val spotDto = portfolioDtos.get(0);
+        for (val openPosition : spotDto.getOpenPositionDtos()) {
+            listOfLists.add(List.of(openPosition.getSymbol(), openPosition.getDeposit()));
+
+        }
+
+        return listOfLists;
     }
 }
