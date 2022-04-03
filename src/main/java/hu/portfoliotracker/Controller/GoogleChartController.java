3d90013b1219a792009b55c84e2dc66199648fa9
Bódi Tamás <h751255@stud.inf.u-szeged.hu>
2022-04-03 19:04:16 +0200

    Create test charts

---
+++ src/main/java/hu/portfoliotracker/Controller/GoogleChartController.java
@@ -0,0 +1,55 @@
+package hu.portfoliotracker.Controller;
+
+import hu.portfoliotracker.DTO.CountCourse;
+import lombok.val;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+@RequestMapping("/google-chart")
+public class GoogleChartController {
+
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
+
+    @GetMapping
+    public String index(Model model) {
+        List<CountCourse> pieChartData = new ArrayList<>();
+        val cc1 = new CountCourse("Humanities", 123);
+        val cc2 = new CountCourse("Sciences", 145);
+        val cc3 = new CountCourse("Other", 67);
+        pieChartData.add(cc1);
+        pieChartData.add(cc2);
+        pieChartData.add(cc3);
+
+        model.addAttribute("chartData", getSpotChartData());
+        return "google-chart";
+    }
+
+    private List<List<Object>> getChartData() {
+        return List.of(
+                List.of("Mushrooms", RANDOM.nextInt(5)),
+                List.of("Onions", RANDOM.nextInt(5)),
+                List.of("Olives", RANDOM.nextInt(5)),
+                List.of("Zucchini", RANDOM.nextInt(5)),
+                List.of("Pepperoni", RANDOM.nextInt(5))
+        );
+    }
+
+    private List<List<Object>> getSpotChartData() {
+        return List.of(
+                List.of("BNB", 1189.33),
+                List.of("BTC", 1498.72),
+                List.of("DOT", 499.83),
+                List.of("ETH", 1000.33),
+                List.of("LINK", 499.56),
+                List.of("MANA", 249.00),
+                List.of("PYR", 249.98)
+        );
+    }
+}
--- src/main/java/hu/portfoliotracker/Controller/PortfolioController.java
+++ src/main/java/hu/portfoliotracker/Controller/PortfolioController.java
@@ -1,5 +1,6 @@
 package hu.portfoliotracker.Controller;
 
+import hu.portfoliotracker.DTO.CountCourse;
 import hu.portfoliotracker.Service.PortfolioService;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping({"/", "/index"})
@@ -23,8 +27,16 @@ public class PortfolioController {
         model.addAttribute("spotBalanceDto", portfolioDtos.get(0));
         model.addAttribute("crossBalanceDto", portfolioDtos.get(1));
         model.addAttribute("isolatedBalanceDto", portfolioDtos.get(2));
-
         model.addAttribute("currency", "$");
+
+        List<CountCourse> pieChartData = new ArrayList<>();
+        val cc1 = new CountCourse("Humanities", 123);
+        val cc2 = new CountCourse("Sciences", 145);
+        val cc3 = new CountCourse("Other", 67);
+        pieChartData.add(cc1);
+        pieChartData.add(cc2);
+        pieChartData.add(cc3);
+        model.addAttribute("pieChartData", pieChartData);
         return "home";
     }
 
